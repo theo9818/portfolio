04bf35a744850d4238750a94d885ab7c7cbe0fed
unknown <theo.paroissin@gmail.com>
2026-06-10 11:58:31 +0200

    corrections fautes

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,7 +4,7 @@
       <div class="row align-items-center">
         <div class="col-md-8">
           <h1 class="display-4 fw-bold mb-3">Bonjour, je suis Ingénieur en Sciences de la Donnée et Intelligence Artificielle</h1>
-          <p class="lead mb-4">Je suis diplômé de l'<a href="https://www.esilv.fr/" target="_blank" rel="noopener noreferrer">ESILV</a> dans la majeur Data/IA, et j'ai suivi une formation de chercheur au sein du laboratoire de l'école. <br>Je suis passionné par l'utilisation de la donnée et la résolution de problèmes complexes que cela permet. Je m'intéresse énormément aux innovations et nouvelles technologies qui permettent de traiter des cas toujours plus complexes. <br>Vous pouvez découvrir mes expériences professionnelles et les différents projets que j'ai mené ci-dessous.</p>
+          <p class="lead mb-4">Je suis diplômé de l'<a href="https://www.esilv.fr/" target="_blank" rel="noopener noreferrer">ESILV</a> dans la majeur Data/IA, et j'ai suivi une formation de chercheur au sein du laboratoire de l'école. <br>Je suis passionné par l'utilisation de la donnée et la résolution de problèmes complexes que cela permet. Je m'intéresse énormément aux innovations et nouvelles technologies qui permettent de traiter des cas toujours plus complexes. <br>Vous pouvez découvrir mes expériences professionnelles et les différents projets que j'ai menés ci-dessous.</p>
           <a href="#experiences" class="btn btn-primary btn-lg me-2 mb-2">Mes expériences</a>
           <a href="#projets" class="btn btn-lg me-2 mb-2" style="background-color: #e0f2fe; color: #0369a1; border: 1px solid #bae6fd;">Mes projets</a>
           <a href="#contact" class="btn btn-outline-light btn-lg me-2 mb-2">Me contacter</a>
